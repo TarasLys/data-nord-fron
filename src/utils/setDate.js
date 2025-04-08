1fb591373d41e85baa16d3d3b5af6b801d4b3f53
TarasLys <opros0609@gmail.com>
2025-04-08 14:12:28 +0200

    888

--- src/utils/setDate.js
+++ src/utils/setDate.js
@@ -20,15 +20,5 @@ export const setDateToSpecificTime = (hour = 14, minute = 0) => {
 
 
 
-// export const setDateToSpecificTime = (hour = 14, minute = 0) => {
-//   const today = new Date();
-//   const specificTime = new Date(
-//     today.getFullYear(),
-//     today.getMonth(),
-//     today.getDate()  -2 //- если добавить = Следующий день
-//   );
-//   specificTime.setHours(hour, minute, 0, 0); // Устанавливаем 14:00
 
-//   return specificTime.toISOString().split("T")[0]; // Возвращаем в формате yyyy-MM-dd
-// };
 
